dbd828fc46a0db811163ec7bdea0161c05008983
przemekWojenka <pdentaw@gmail.com>
2022-06-03 18:22:00 +0200

    function for finding files created

--- altScrapper.py
+++ altScrapper.py
@@ -1,1 +1,12 @@
-#hello 2sd commit
+import os
+
+
+def get_file_path(directory, filenames=None):
+    if filenames is None:
+        filenames = []
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        filenames.append(f) if os.path.isfile(f) else get_file_path(f, filenames)
+    return filenames
+
+
