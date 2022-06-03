cbdc2ac6adc17f68d2d81cd41f60126a02c02ba1
przemekWojenka <pdentaw@gmail.com>
2022-06-03 20:39:26 +0200

    returning list of words in file

--- altScrapper.py
+++ altScrapper.py
@@ -1,14 +1,14 @@
 import os
 
-directory = '/Users/p.wojenka/projekty/service-b2c/service-b2c/src/sections'
+directory = '/Users/p.wojenka/projekty/service-b2c/service-b2c/src/components'
 
 
-def get_file_path(directory, filenames=None):
+def get_files_path(directory, filenames=None):
     if filenames is None:
         filenames = []
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
-        filenames.append(f) if os.path.isfile(f) else get_file_path(f, filenames)
+        filenames.append(f) if os.path.isfile(f) else get_files_path(f, filenames)
     return filenames
 
 
@@ -16,14 +16,15 @@ def is_alt(elem):
     return elem == 'alt='
 
 
-def split_file_into_letters(file):
+def split_file_into_words(file):
     f = open(file, 'r')
-    for word in f:
-        print(word.split())
+    file_words = []
+    for line in f:
+        file_words.append(line.split())
+    print(file_words)
     f.close()
 
-for i in get_file_path(directory):
-    print(split_file_into_letters(i))
+
 
 
 
