04f7f37d02a2c27dde87fd5b49ae1c23887a1962
przemekWojenka <pdentaw@gmail.com>
2022-06-03 19:09:05 +0200

    add get_file_path, split_into_letter

--- altScrapper.py
+++ altScrapper.py
@@ -1,5 +1,7 @@
 import os
 
+directory = '/Users/p.wojenka/projekty/service-b2c/service-b2c/src/sections'
+
 
 def get_file_path(directory, filenames=None):
     if filenames is None:
@@ -8,5 +10,20 @@ def get_file_path(directory, filenames=None):
         f = os.path.join(directory, filename)
         filenames.append(f) if os.path.isfile(f) else get_file_path(f, filenames)
     return filenames
+
+
+def is_alt(elem):
+    return elem == 'alt='
+
+
+def split_file_into_letters(file):
+    f = open(file, 'r')
+    for word in f:
+        print(word.split())
+    f.close()
+
+for i in get_file_path(directory):
+    print(split_file_into_letters(i))
+
 
 
