191d5767a904c8f0303ea0fda76bd698cfc706ad
przemekWojenka <pdentaw@gmail.com>
2022-06-06 12:49:43 +0200

    finish - only bug to repair

--- altScrapper.py
+++ altScrapper.py
@@ -1,6 +1,6 @@
+import json
 import os
-
-directory = '/Users/p.wojenka/projekty/service-b2c/service-b2c/src/components'
+import io
 
 
 def get_files_path(directory, filenames=None):
@@ -13,11 +13,14 @@ def get_files_path(directory, filenames=None):
 
 
 def split_file_into_words(file):
-    f = open(file, 'r')
     file_words = []
-    for line in f:
-        file_words.append(line.split())
-    f.close()
+    if file.endswith('.yaml') or file.endswith('.php') or file.endswith('.html') or file.endswith('.twig'):
+        with io.open(file, 'r', encoding='windows-1252') as f:
+            for line in f:
+                file_words.append(line.split())
+            f.close()
+    else:
+        pass
     return file_words
 
 
@@ -37,37 +40,38 @@ def get_file_alts(list_of_list_words):
 
 
 def alt_filter(word):
-    return word.replace("alt=", '')
+    return word.replace("alt=", '').replace("'", "").replace('"', '')
 
 
 def get_alts(directory):
     file_paths = get_files_path(directory)
-    alts = []
+    dicti = {}
 
     for file in file_paths:
         file_words = split_file_into_words(file)
-        temporary = file
         if get_file_alts(file_words):
-            print(file)
-            #TODO: czemu to printuje pojedyncz litery jak dam append?
-            alts.append(get_file_alts(file_words))
-    return alts
+            arr = []
+            for i in get_file_alts(file_words):
+                arr.append(file)
+                dicti.update({alt_filter(i): f'{arr}'})
+    return dicti
 
 
-def alts_filter(directory):
-    list_of_alts = get_alts(directory)
-    clear_alts = []
-    for ls in list_of_alts:
-        for alt in ls:
-            clear_alts.append(alt_filter(alt))
-    print(clear_alts)
-    return clear_alts
+def save_json(dictionary, json_path, json_name):
+    f = open(json_path + json_name, "w")
+    json.dump(dictionary, f)
+    f.close()
 
 
-alts_filter(directory)
+def main(directory, json_path, json_name):
+    save_json(get_alts(directory), json_path, json_name)
 
 
+directory = '/Users/p.wojenka/projekty/untitled/baltics-ecommerce/templates'
+json_path = '/Users/p.wojenka/projekty/altScrapper/'
+json_name = 'alts.json'
 
+# add file extensions to read in split_file_into_words
+#TODO: naprawic buga z encoding
 
-
-
+main(directory, json_path, json_name)
